23899d72a922c923924d907eedfd7512baca0b3f
xuzhen <zhenx@zju.edu.cn>
2022-10-13 19:07:53 +0800

    feat: modify image link scripts

--- merge_images_to_one_dir.py
+++ merge_images_to_one_dir.py
@@ -1,31 +1,46 @@
 import os
+import sys
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('path', type=str) # the root folder, whose images folder will be recursively liked to output
-parser.add_argument('output', type=str) # the output folder
-args = parser.parse_args()
-
-from termcolor import cprint, colored
 from os.path import join
+from termcolor import cprint, colored
 
-def run(cmd: str):
-    print(colored('Run: ', 'blue') + cmd)
-    os.system(cmd)
 
-run(f'mkdir -p {args.output}')
+def run(cmd):
+    func = sys._getframe(1).f_code.co_name
+    print(colored(func, 'yellow')+": "+colored(cmd, 'blue'))
+    code = os.system(cmd)
+    if code != 0:
+        raise RuntimeError(colored(str(code), 'red')+" <- "+colored(func, 'yellow')+": "+colored(cmd, 'red'))
 
-# path: should contain multiple humans, images in humans, cameras in images, and actual image in cameras
-for human in os.listdir(args.path):
-    human_dir = join(args.path, human)
-    img_dir = join(human_dir, 'images')
-    for cam in os.listdir(img_dir):
-        cam_dir = join(img_dir, cam)
-        if cam_dir == args.output:
-            continue
-        for img in os.listdir(cam_dir):
-            img_path = join(cam_dir, img)
-            new_img = f"{human}.{cam}.{img}" # like F1_06_000000.02.000000.jpg
-            new_img_path = join(args.output, new_img)
-            run(f'ln -s {img_path} {new_img_path}')
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)  # the root folder, whose images folder will be recursively liked to output
+    parser.add_argument('output', type=str)  # the output folder
+    parser.add_argument('--one_human', action='store_true')
+    args = parser.parse_args()
+
+    run(f'mkdir -p {args.output}')
+
+    humans = os.listdir(args.path)
+    if args.one_human:  # replace human
+        humans = [os.path.split(args.path)[-1]]
+        args.path = args.path.replace(humans[0], '')
+
+    # path: should contain multiple humans, images in humans, cameras in images, and actual image in cameras
+    for human in humans:
+        human_dir = join(args.path, human)
+        img_dir = join(human_dir, 'images')
+        for cam in sorted(os.listdir(img_dir)):
+            cam_dir = join(img_dir, cam)
+            if cam_dir == args.output:
+                continue
+            for img in sorted(os.listdir(cam_dir)):
+                img_path = join(cam_dir, img)
+                new_img = f"{human}.{cam}.{img}"  # like F1_06_000000.02.000000.jpg
+                new_img_path = join(args.output, new_img)
+                run(f'ln -s {img_path} {new_img_path}')
+
+
+if __name__ == '__main__':
+    main()
--- split_images_from_one_dir.py
+++ split_images_from_one_dir.py
@@ -1,35 +1,39 @@
 import os
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('path', type=str) # the root folder, whose images folder will be recursively liked to output
-parser.add_argument('output', type=str) # the output folder
-parser.add_argument('--mask_dir', default='mask')
-args = parser.parse_args()
-
-from termcolor import cprint, colored
 from os.path import join
-
-def run(cmd: str):
-    print(colored('Run: ', 'blue') + cmd)
-    os.system(cmd)
+from .merge_images_to_one_dir import run
 
 
-# path: should contain multiple humans, images in humans, cameras in images, and actual image in cameras
-for human in os.listdir(args.path):
-    human_dir = join(args.path, human)
-    img_dir = join(human_dir, 'images') # copy target (split destination)
-    msk_dir = join(human_dir, args.mask_dir) # eval results (to be split)
-    for cam in os.listdir(img_dir):
-        cam_dir = join(img_dir, cam) # reference: original image dir
-        if cam_dir == args.output.replace(args.mask_dir, 'images'):
-            continue
-        for img in os.listdir(cam_dir):
-            msk = img.replace('.jpg', '.png')
-            new_msk = f"{human}.{cam}.{msk}" # like F1_06_000000.02.000000.jpg
-            msk_cam_dir = join(msk_dir, cam)
-            msk_path = join(msk_cam_dir, msk) # copy result like (...)F1_06_000000/images/02/000000.png
-            new_msk_path = join(args.output, new_msk) # copy from (to be split)
-            run(f'mkdir -p {msk_cam_dir}')
-            run(f'mv {new_msk_path} {msk_path}')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)  # the root folder, whose images folder will be recursively liked to output
+    parser.add_argument('output', type=str)  # the output folder
+    parser.add_argument('--mask_dir', default='mask')
+    parser.add_argument('--one_human', action='store_true')
+    args = parser.parse_args()
 
+    humans = os.listdir(args.path)
+    if args.one_human:  # replace human
+        humans = [os.path.split(args.path)[-1]]
+        args.path = args.path.replace(humans[0], '')
+    # path: should contain multiple humans, images in humans, cameras in images, and actual image in cameras
+    for human in humans:
+        human_dir = join(args.path, human)
+        img_dir = join(human_dir, 'images')  # copy target (split destination)
+        msk_dir = join(human_dir, args.mask_dir)  # eval results (to be split)
+        for cam in sorted(os.listdir(img_dir)):
+            cam_dir = join(img_dir, cam)  # reference: original image dir
+            if cam_dir == args.output.replace(args.mask_dir, 'images'):
+                continue
+            for img in sorted(os.listdir(cam_dir)):
+                msk = img.replace('.jpg', '.png')
+                new_msk = f"{human}.{cam}.{msk}"  # like F1_06_000000.02.000000.jpg
+                msk_cam_dir = join(msk_dir, cam)
+                msk_path = join(msk_cam_dir, msk)  # copy result like (...)F1_06_000000/images/02/000000.png
+                new_msk_path = join(args.output, new_msk)  # copy from (to be split)
+                run(f'mkdir -p {msk_cam_dir}')
+                run(f'mv {new_msk_path} {msk_path}')
+
+
+if __name__ == '__main__':
+    main()
